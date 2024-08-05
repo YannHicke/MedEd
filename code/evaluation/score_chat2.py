f821d2f314966e8fbaa63873a0e6342703abf731
Yann Hicke <ylh8@cornell.edu>
2024-08-05 19:32:23 -0400

    Add fireworks api

--- code/evaluation/score_chat2.py
+++ code/evaluation/score_chat2.py
@@ -5,7 +5,8 @@ import json
 import pandas as pd
 from checklist_prompts import checklist
 from utils_apis import (
-    api_call_map, 
+    api_call_map,
+    parse_call_map, 
     parse_json, 
     parse_string, 
     get_prompts_call_map
@@ -14,46 +15,65 @@ import yaml
     
 prompt_map = {
     "openai": "json",
-    "gemini": "string",
+    "gemini": "json",
     "anthropic": "string",
     "cohere": "string",
-    "together": "string"
+    "together": "json",
+    "fireworks": "json"
 }
 
 def score_checklist(transcript, category, items):
     pass
 
-def score_mirs(transcript, examples, model_list):
+def score_mirs(transcript, examples, model_list, file_name):
     scores = {}
     explanations = {}
     for model in model_list:
         scores[model] = {}
         explanations[model] = {}
 
+    with open(f"./mapping_items_to_score.json", 'r') as file:
+        mapping_items_to_score = json.load(file)
+
     json_prompts = get_prompts_call_map(examples, "json")
     for item, _ in json_prompts.items():
+        # if item not in mapping_items_to_score[file_name]:
+        #     for model in model_list:
+        #         scores[model][item] = "Not scored"
+        #         explanations[model][item] = "Not scored"
+        #         print(f"Skipping item: {item} for model: {model}")
+        #     continue
         print(f"\n\nItem: {item}")
         for model in model_list:
             print(f"\n\nModel: {model}")
             # Define prompt, api call, and parse response
             prompt = get_prompts_call_map(examples, prompt_map[model])[item]
+            # prompt = get_prompts_call_map(examples, "json")[item]
             api_call = api_call_map.get(model)
-            parse_response = parse_json if prompt_map[model] == "json" else parse_string
+            parse_response_model = parse_call_map.get(model)
+            parse_response = (
+                (lambda response: parse_json(parse_response_model(response)))
+                if prompt_map[model] == "json"
+                else (lambda response: parse_string(parse_response_model(response)))
+            )            
 
             # Call the API and parse response
             response = api_call(transcript, prompt)
             score, explanation = parse_response(response)
+            breakpoint()
 
             # print results
             print(f"\n\nScore: {score}, \n\nExplanation: {explanation}")
+            # print(f"\n\nPrompt: {prompt}")
 
             # Store in scores and explanations
             scores[model][item] = score
             explanations[model][item] = explanation
+            breakpoint()
 
 
     # Store results into a dataframe
-    results = pd.DataFrame(scores[model_list[0]], orient="index", columns=f"scores_{model_list[0]}")
+    results = pd.DataFrame.from_dict(scores[model_list[0]], orient="index", columns=[f"scores_{model_list[0]}"])
     results[f"explanations_{model_list[0]}"] = pd.Series(explanations[model_list[0]])
     for model in model_list[1:]:
         results[f"scores_{model}"] = pd.Series(scores[model])
@@ -61,7 +81,7 @@ def score_mirs(transcript, examples, model_list):
 
 
     # Save the dataframe to a csv file
-    results.to_csv("./results/mirs_scores.csv")
+    results.to_csv(f"./results/mirs_scores_{file_name}_test_gemini.csv")
 
 
 if __name__ == "__main__":
@@ -71,25 +91,30 @@ if __name__ == "__main__":
         config = yaml.safe_load(file)
 
     # Check if the conversation file exists
-    file_path = os.path.join("../simulation/conversation_history/UConn/", config["conversation_file"])
-    if not os.path.exists(file_path):
-        print(f"Error: The file '{config['conversation_file']}' does not exist.")
-        sys.exit(1)
+    for file_name in config["conversation_files"]:
+        file_path = os.path.join("../simulation/conversation_history/UConn/", file_name)
+        if not os.path.exists(file_path):
+            print(f"Error: The file '{file_name}' does not exist.")
+            sys.exit(1)
 
     # Load the transcript
-    with open(file_path, 'r') as file:
-        transcript = file.read().strip()
+    for file_name in config["conversation_files"]:
+        file_path = os.path.join("../simulation/conversation_history/UConn/", file_name)
+        with open(file_path, 'r') as file:
+            transcript = file.read().strip()
 
-    transcript = transcript.replace("User:", "Physician:").replace("Assistant:", "Patient:") #TODO: make it more systematic
+        transcript = transcript.replace("User:", "Physician:").replace("Assistant:", "Patient:") #TODO: make it more systematic
 
-    # Print the configuration
-    print("\n\n############################################################")
-    print("Scoring conversation file: ", config["conversation_file"])
-    print("Examples provided: ", config["examples"])
-    print("Score conversation with Mirs rubric: ", config["mirs"])
-    print("Score conversation with checklist: ", config["checklist"])
-    print("Model List: ", config["model_list"])
-    print("############################################################\n\n")
+        # Print the configuration
+        print("\n\n############################################################")
+        print("Scoring conversation file: ", file_name)
+        print("Examples provided: ", config["examples"])
+        print("Score conversation with Mirs rubric: ", config["mirs"])
+        print("Score conversation with checklist: ", config["checklist"])
+        print("Model List: ", config["model_list"])
+        print("############################################################\n\n")
 
-    if config["mirs"]:
-        score_mirs(transcript, config["examples"], config["model_list"])
+        if config["mirs"]:
+            score_mirs(transcript, config["examples"], config["model_list"], file_name)
+
+    print("All conversations have been scored.")
--- code/evaluation/utils_apis.py
+++ code/evaluation/utils_apis.py
@@ -1,6 +1,7 @@
 from together import Together
 from openai import OpenAI
 import google.generativeai as genai
+from fireworks.client import Fireworks
 import anthropic
 import cohere
 import time
@@ -13,12 +14,16 @@ from nojson_mirs_prompts import nojson_mirs_prompts
 from mirs_prompts_noExamples import mirs_prompts_noExamples
 from nojson_mirs_prompts_noExamples import nojson_mirs_prompts_noExamples
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # initialize models
 client_openai = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 client_together = Together(api_key=os.environ.get("TOGETHER_API_KEY"))
 genai.configure(api_key=os.environ.get("GOOGLE_API_KEY"))
 client_anthropic = anthropic.Anthropic(api_key=os.environ["ANTHROPIC_API_KEY"])
 co = cohere.Client(os.environ["COHERE_API_KEY"])
+client_fireworks = Fireworks(api_key=os.environ["FIREWORKS_API_KEY"])
 
 
 def openai_api_call(transcript, prompt):
@@ -28,6 +33,7 @@ def openai_api_call(transcript, prompt):
         messages=[{"role": "system", "content": prompt},
                 {"role": "user", "content": transcript}]
     )
+    # print(prompt)
     return response_openai
 
 
@@ -52,13 +58,29 @@ def gemini_api_call(transcript, prompt):
     return response_gemini
 
 
+claude_sys_prompt = """
+You are an expert evaluator of medical interviews conducted by medical students. 
+Your task is to analyze interview transcripts and provide detailed assessments on various aspects of the interview process. 
+For each evaluation task, you will:
+
+1. Carefully read the provided transcript.
+2. Analyze specific aspects of the interview as requested.
+3. Provide a detailed reasoning for your evaluation, citing specific examples from the transcript.
+4. Assign a score based on the given rubric.
+"""
+
 def anthropic_api_call(transcript, prompt):
+    # print("system prompt\n\n", claude_sys_prompt)
+    # print("messages\n\n", prompt.replace("{TRANSCRIPT}", transcript))
+    # print("Start Assistant message\n\n", "I've carefully reviewed the provided medical interview transcript. Based on the evaluation criteria outlined in the prompt, I'll now provide a detailed analysis and scoring. Let's begin with the reasoning for my evaluation:")
+    breakpoint()
     response_anthropic = client_anthropic.messages.create(
-        model="claude-3-opus-20240229",
+        model="claude-3-5-sonnet-20240620",
         max_tokens=1024,
-        system=prompt,
+        system=claude_sys_prompt,
         messages=[
-            {"role": "user", "content": transcript} 
+            {"role": "user", "content": prompt.replace("{TRANSCRIPT}", transcript)},
+            {"role": "assistant", "content": "I've carefully reviewed the provided medical interview transcript. Based on the evaluation criteria outlined in the prompt, I'll now provide a detailed analysis and scoring. Let's begin with the reasoning for my evaluation:"} 
         ]
     )
     return response_anthropic
@@ -82,26 +104,58 @@ def together_api_call(transcript, prompt):
     
     return response_together
 
+def fireworks_api_call(transcript, prompt):
+    response_fireworks = client_fireworks.chat.completions.create(
+        model="accounts/fireworks/models/llama-v3p1-405b-instruct",
+        messages=[{"role": "user", "content": prompt},
+        {"role": "user", "content": transcript}]
+    )
+    return response_fireworks
+
 
 def parse_json(response):
     try:
-        score = json.loads(response.choices[0].message.content)["score"]
-        explanation = json.loads(response.choices[0].message.content)["explanation"]
+        score = json.loads(response)["score"]
     except:
         score = "N/A"
-        explanation = response.choices[0].message.content
+    try:
+        explanation = json.loads(response)["justification"]
+    except:
+        explanation = response
 
     return score, explanation
+
 
 def parse_string(response):
     try:
-        score = response.text.split("Score: ")[1].split("\n")[0]
-        explanation = response.text.split("Explanation: ")[1].split("\n")[0]
+        score = response.split("<score>")[1].split("</score>")[0]
     except:
         score = "N/A"
-        explanation = response.text
+    try:
+        explanation = response.split("<reasoning>")[1].split("</reasoning>")[0]
+    except:
+        explanation = response
 
     return score, explanation
+
+def parse_openai(response):
+    return response.choices[0].message.content
+
+def parse_together(response):
+    return response.choices[0].message.content
+
+def parse_anthropic(response):
+    return response.content[0].text
+
+def parse_gemini(response):
+    return response.text
+
+def parse_cohere(response):
+    return response.text
+
+def parse_fireworks(response):
+    return response.choices[0].message.content
+
 
 
 api_call_map = {
@@ -109,7 +163,17 @@ api_call_map = {
     "gemini": gemini_api_call,
     "anthropic": anthropic_api_call,
     "cohere": cohere_api_call,
-    "together": together_api_call
+    "together": together_api_call,
+    "fireworks": fireworks_api_call
+}
+
+parse_call_map = {
+    "openai": parse_openai,
+    "gemini": parse_gemini,
+    "anthropic": parse_anthropic,
+    "cohere": parse_cohere,
+    "together": parse_together,
+    "fireworks": parse_fireworks
 }
 
 def get_prompts_call_map(examples, prompt_type):
